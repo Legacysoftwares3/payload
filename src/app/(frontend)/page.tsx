b192ba55fa6249dee749a8244bc2a9d65b9d0e57
Darryn Tannous <darryntannous@gmail.com>
2025-11-08 12:42:49 -0500

    change 1
    test

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -27,7 +27,7 @@ export default async function HomePage() {
             width={65}
           />
         </picture>
-        {!user && <h1>Welcome to your new project.</h1>}
+        {!user && <h1>Welcome to your new Meybohm Backend.</h1>}
         {user && <h1>Welcome back, {user.email}</h1>}
         <div className="links">
           <a
@@ -44,16 +44,18 @@ export default async function HomePage() {
             rel="noopener noreferrer"
             target="_blank"
           >
-            Documentation
+            Maybohm Comercial
           </a>
         </div>
       </div>
+      {/*
       <div className="footer">
         <p>Update this page by editing</p>
         <a className="codeLink" href={fileURL}>
           <code>app/(frontend)/page.tsx</code>
         </a>
       </div>
+      */}
     </div>
   )
 }
